45733db8e29282bf69ce1c922320ee9f7a9d4814
Andreas Müller <schnitzeltony@gmail.com>
2023-01-30 21:41:39 +0100

    zenux_device_detect.h: Fix include guard
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- board/variscite/mx6var_som/zenux_device_detect.h
+++ board/variscite/mx6var_som/zenux_device_detect.h
@@ -1,8 +1,8 @@
-#ifndef __ZENUX_MCONTROLLER_H
-#define __ZENUX_MCONTROLLER_H
+#ifndef __ZENUX_DEVICE_DETECT_H
+#define __ZENUX_DEVICE_DETECT_H
 
 #ifndef CONFIG_SPL_BUILD
 
 #endif /* CONFIG_SPL_BUILD */
 
-#endif /* __ZENUX_MCONTROLLER_H */
+#endif /* __ZENUX_DEVICE_DETECT_H */
