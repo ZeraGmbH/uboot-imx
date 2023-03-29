1daa75c1eb236ba1c9fa336f6032b932f64c3a8b
Andreas Müller <schnitzeltony@gmail.com>
2023-03-29 11:44:32 +0200

    zenux_mcontroller: Implement read data
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- board/variscite/mx6var_som/zenux_mcontroller_io.c
+++ board/variscite/mx6var_som/zenux_mcontroller_io.c
@@ -6,17 +6,27 @@ static u16 generateCmdRequest(u16 cmdId, u8 subDevice, u8* param, u16 paramLen, 
 
 u16 readCmd(uint i2cAddr, u16 cmdId, u8 *readBuff)
 {
-    u8 requestBuff[128];
+    u8 requestBuff[16];
     u8 requestResponse[5];
     u16 bytesRead = 0;
     u16 reqLen = generateCmdRequest(cmdId, 0, NULL, 0, requestBuff);
 
-    if(!i2c_write(i2cAddr, 0, 0, requestBuff, reqLen)) {
-        if(!i2c_read(i2cAddr, 0, -1, requestResponse, 5)) {
+    if(!i2c_write(i2cAddr, 0, 0, requestBuff, reqLen)) { // cmd -> ctl
+        if(!i2c_read(i2cAddr, 0, -1, requestResponse, 5)) { // <- ctl errmask/len
             bytesRead = decodeRequestResponse(requestResponse);
+            if(bytesRead > 128) {
+                puts("Cannot not read more than 128 bytes!\n");
+                bytesRead = 0;
+            }
+            if(bytesRead == 0)
+                printf("No data to read - is comannd ID %04X correct?\n", cmdId);
+            else if(i2c_read(i2cAddr, 0, -1, readBuff, bytesRead)) { // <- ctl data
+                puts("Could not read data from I2c!\n");
+                bytesRead = 0;
+            }
         }
         else
-            puts("Could not read to I2c!\n");
+            puts("Could not read error mask/length from I2c!\n");
     }
     else
         puts("Could not write to I2c!\n");
